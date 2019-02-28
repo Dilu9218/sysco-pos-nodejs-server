5b0d6377b783aa9a107169fa2c232542f3bb0011
CloudyPadmal <blog.padmal@gmail.com>
2019-03-01 00:17:09 +0530

    shift to production URL

--- src/database/database.js
+++ src/database/database.js
@@ -8,8 +8,8 @@ class Database {
     }
 
     connectToMongo() {
-        mongoose.connect('mongodb://localhost:27017/posdb', { useNewUrlParser: true })
-        //mongoose.connect('mongodb+srv://Padmal:abcd@1234@syscoposcluster-triov.mongodb.net/test?retryWrites=true', { useNewUrlParser: true })
+        //mongoose.connect('mongodb://localhost:27017/posdb', { useNewUrlParser: true })
+        mongoose.connect('mongodb+srv://Padmal:abcd@1234@syscoposcluster-triov.mongodb.net/test?retryWrites=true', { useNewUrlParser: true })
             .then(res => { });
     }
 }
