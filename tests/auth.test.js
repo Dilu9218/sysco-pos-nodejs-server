eb568d09c277e56daade90929f21bb8216dc8ea6
Dilu9218 <dilu9218@gmail.com>
2020-10-01 10:11:08 +0300

    feat: test for auth

---
+++ tests/auth.test.js
@@ -0,0 +1,26 @@
+var jwt = require("jsonwebtoken");
+var config = require("./config");
+var user = require("../database/models/user.model");
+
+function ValidateToken(req, res, next) {
+    var token = req.headers["x-access-token"];
+    if (!token) {
+        return res.status(403).send({ error: "Token not provided" });
+    } else {
+        jwt.verify(token, config.secret, function (err, decoded) {
+            if (err) {
+                return res.status(500).send({ error: "Token is invalid" });
+            } else {
+                user.findById(decoded.id).then((doc) => {
+                    req._id = doc._id;
+                    req._admin = doc.isAdmin;
+                    next();
+                }).catch((err) => {
+                    return res.status(404).send({ error: "User not found" });
+                });
+            }
+        });
+    }
+}
+
+module.exports = ValidateToken;
